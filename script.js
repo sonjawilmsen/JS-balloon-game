e606c91a22cda3249243ad657efea3fcdc01dff1
Sonja <s_wilmsen@hotmail.com>
2016-11-20 15:12:40 +0100

    changed the animation

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ var counter = 0;
 
 
 function start () {
-  for(var i=0; i<10; i++){
+  for(var i=0; i<20; i++){
     var balloonCopy = balloon.clone();
-    balloonCopy.css({left: i*200});
+    balloonCopy.css({left: i*100});
     balloonCopy.appendTo("body");
+
     balloonCopy.click(function() {
       pop_sound.play();
       $(this).remove();
       counter = counter + 1;
       $(".counter").html(counter);
     });
-    balloonCopy.animate({bottom:"100%"}, 8000);
+    balloonCopy.animate({bottom: "100%", left: Math.random() * 100 + "%" }, 16000 - Math.random() * 4000);
     };
   balloon.remove();
 }
@@ -23,7 +24,7 @@ function start () {
 function preloadPopSound(){
   var audio = new Audio('fireball.wav');
   audio.preload = "auto";
-  $(audio).on("loadeddata", start);  // jQuery checking
+  $(audio).on("loadeddata", start);
   return audio;
 }
 var pop_sound = preloadPopSound();

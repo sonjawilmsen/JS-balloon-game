0ce0346c2956f8c610e1734348b805c84a617559
Sonja <s_wilmsen@hotmail.com>
2016-11-20 12:10:21 +0100

    jquery lib version try out

---
+++ script.js
@@ -0,0 +1,33 @@
+var balloon = $(".balloon");
+var counter = 0;
+
+
+function start () {
+  for(var i=0; i<10; i++){
+    var balloonCopy = balloon.clone();
+    balloonCopy.css({left: i*200});
+    balloonCopy.appendTo("body");
+    balloonCopy.click(function() {
+      pop_sound.play();
+      $(this).remove();
+      counter = counter + 1;
+      $(".counter").html(counter);
+    });
+    balloonCopy.animate({bottom:"100%"}, 8000);
+    };
+  balloon.remove();
+}
+
+
+// balloon_copy.click(function(){
+//           pop_sound.play();
+//
+
+
+function preloadPopSound(){
+  var audio = new Audio('fireball.wav');
+  audio.preload = "auto";
+  $(audio).on("loadeddata", start);  // jQuery checking
+  return audio;
+}
+var pop_sound = preloadPopSound();
